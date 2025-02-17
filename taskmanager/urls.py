6882e78899254f10e6cc9a71984b6a3c4e01dfd1
jdomdev <jdomingodev@duck.com>
2025-02-17 20:12:35 +0100

    feat: add task list url

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-
+from tasks.views import task_list
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", task_list, name="task-list"),
 ]
